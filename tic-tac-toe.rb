5ea458e0bef8f30ec96cbd9a458cec8e31d0982a
Jason Kearney <jkearney345@gmail.com>
2022-08-08 15:38:31 -0400

    make positions

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,19 +1,38 @@
 class Board
-  def initialize(player1, player2)
-    @new_board = ""
-    @player1 = Player.new(player1, black)
-    @player2 = Player.new(player2, red)
+
+  def initialize()
+    @new_board = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
+    create_players()
+  end
+
+  def create_players()
+    @player1 = Player.new("Player1")
+    @player2 = Player.new("Player2")
   end
 end
 
 class Player < Board
-  def initialize(name, color)
+  def initialize(name)
     @name = name
-    @color = color
   end
 end
 
-board = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
-board.each do |row|
-  p row
-end
+positions = {
+  1 => "[0][0]",
+  2 => "[0][1]",
+  3 => "[0][2]",
+  4 => "[1][0]",
+  5 => "[1][1]",
+  6 => "[1][2]",
+  7 => "[2][0]",
+  8 => "[2][1]",
+  9 => "[2][2]",
+}
+
+p positions[1]
+
+# board.each do |row|
+#   p row
+# end
+
+game = Board.new
